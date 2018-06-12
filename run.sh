767d119d3bd17760a22d6c309a8f1e7130d07ddf
Philipp Wollschlegel <folken@kabelsalat.ch>
2018-06-12 14:09:39 +0200

    compile for all supported platforms when executing ./run

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run -it -v /opt/xcsoar:/opt/xcsoar xcsoar:latest xcsoar-compile-unix 
+docker run -it -v /opt/xcsoar:/opt/xcsoar xcsoar:latest xcsoar-update
+docker run -it -v /opt/xcsoar:/opt/xcsoar xcsoar:latest xcsoar-compile-unix
+docker run -it -v /opt/xcsoar:/opt/xcsoar xcsoar:latest xcsoar-compile-android
