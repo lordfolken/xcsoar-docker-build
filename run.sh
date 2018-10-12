9a93445029950904fab7333fd9312052199c3a48
Philipp Wollschlegel <folken@kabelsalat.ch>
2018-10-13 00:14:27 +0200

    add support for pc target

--- run.sh
+++ run.sh
@@ -8,3 +8,4 @@ mkdir /opt/xcsoar 2> /dev/null
 docker run -it $VOLUMES folken/xcsoar-build:$VERSION xcsoar-update
 docker run -it $VOLUMES folken/xcsoar-build:$VERSION xcsoar-compile-unix
 docker run -it $VOLUMES folken/xcsoar-build:$VERSION xcsoar-compile-android
+docker run -it $VOLUMES folken/xcsoar-build:$VERSION xcsoar-compile-pc
